0967713878b2cf514f807ca4ad917303e0f3dd28
Hamit Yuvaci <yvchamit@gmail.com>
2026-03-09 08:30:02 -0500

    1-Proje setup tamamlandı.

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Login Formu E2E Testi</h1>
+      <div className="card">
+        <button onClick="">
+          Let's Start
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
